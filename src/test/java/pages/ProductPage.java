7161291bae9863a5a465d22eaa9b14bd8a043555
Deeba <deebashok2425@gmail.com>
2026-02-09 17:12:53 +0530

    First Commit

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -8,6 +8,6 @@ public class ProductPage extends BasePage {
 
     public void addProductToCart() {
         driver.findElement(addToCartButton).click();
-        System.out.println("The product was added");
+        System.out.println("The product was add");
     }
 }
